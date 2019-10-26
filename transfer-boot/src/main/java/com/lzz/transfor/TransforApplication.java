60ee6f4fc36634686d16ab8b36a9fd81bd59a3b3
lizhongze <password>
2019-10-26 19:53:36 +0800

    新建项目

---
+++ transfer-boot/src/main/java/com/lzz/transfor/TransforApplication.java
@@ -0,0 +1,13 @@
+package com.lzz.transfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransforApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransforApplication.class, args);
+    }
+
+}
---
+++ transfer-boot/src/test/java/com/lzz/transfor/TransforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzz.transfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransforApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
